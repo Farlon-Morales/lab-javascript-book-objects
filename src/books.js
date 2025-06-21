41655b50ac7ee0340dc05be50f78c4315ec653f6
Farlon Morales <moralesaguiar@live.com>
2025-06-21 17:13:01 +0200

    https://github.com/ironhack-labs/lab-javascript-book-objects.git

--- src/books.js
+++ src/books.js
@@ -38,15 +38,52 @@
 
 
 // Your code here:
-const booksArray = [];
-
+const booksArray = [
+  { // Book1
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+  },
+  { // Book2
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+    }
+  },
+  {  // Book3
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention..."
+    }
+  },
+  { /// Book4 
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+    }
+  }
+];
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+function getBookDetails(bookDetails) {
+  const msg = bookDetails.title + " - " + bookDetails.author + " - " + bookDetails.pages + " pages"
+  console.log(msg)
+  return msg
 }
 
 
@@ -54,12 +91,23 @@ function getBookDetails() {
 // Iteration 3 | Delete Language
 // Your code here:
 
-
-
+for (let i = 0; i < booksArray.length; i++) {
+  delete booksArray[i].details.language
+}
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+function addReadingTime(readingTime) {
+  for (let i = 0; i < booksArray.length; i++) {
+    const pagesBook = booksArray[i].pages
 
+    const readingTime = Math.ceil((pagesBook * 500) / 90)
+    
+    booksArray[i].readingTime = readingTime
+
+  }
+
+}
 
 
 
@@ -69,26 +117,28 @@ function getBookDetails() {
  The book info is stored in arrays with structure: [title, pages]. 
 */
 const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
+  "J. K. Rowling": [
+    ["Harry Potter and the Philosopher's Stone", 223],
+    ["Harry Potter and the Chamber of Secrets", 251],
+    ["Harry Potter and the Prisoner of Azkaban", 317],
+    ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+    ["Cryptonomicon", 928],
+    ["Anathem", 1008],
+    ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+    ["Outliers", 320],
+    ["Blink", 287],
+  ],
 };
 
 function booksByAuthor() {
   // Your code here:
-  
+for ( i = 0; i < dictionary.length; 1++) {
+  return dictionary[0]
+}
 }
 
 
@@ -96,5 +146,5 @@ function booksByAuthor() {
 // Bonus: Iteration 6 | Average Page Count
 function averagePageCount() {
   // Your code here:
-  
+
 }
